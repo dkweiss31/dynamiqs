2c80dae3e6d3dbd29f5beea88ef00983342adaef
Danny <dkweiss@u.northwestern.edu>
2024-03-27 11:39:48 -0400

    max amp use diffrax cubic spline plot pops for dynamic chi

--- examples/run_dynamic_chi.py
+++ examples/run_dynamic_chi.py
@@ -3,29 +3,30 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
 import optax
-from jax_cosmo.scipy.interpolate import InterpolatedUnivariateSpline
 
 from dynamiqs import Options, grape, timecallable, dag, tensor, basis, destroy, eye, unit
+from dynamiqs import sesolve
 from dynamiqs.utils.file_io import generate_file_path
+import diffrax as dx
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="dynamic chi sim")
     parser.add_argument("--idx", default=-1, type=int, help="idx to scan over")
-    parser.add_argument("--gate", default="error_parity", type=str,
+    parser.add_argument("--gate", default="error_parity_plus", type=str,
                         help="type of gate. Can be error_parity_g, error_parity_plus, ...")
-    parser.add_argument("--c_dim", default=3, type=int, help="cavity hilbert dim cutoff")
-    parser.add_argument("--t_dim", default=3, type=int, help="tmon hilbert dim cutoff")
+    parser.add_argument("--c_dim", default=4, type=int, help="cavity hilbert dim cutoff")
+    parser.add_argument("--t_dim", default=4, type=int, help="tmon hilbert dim cutoff")
     parser.add_argument("--Kerr", default=0.100, type=float, help="transmon Kerr in GHz")
-    parser.add_argument("--chi_max", default=0.005, type=float, help="max chi drive in GHz")
-    parser.add_argument("--dt", default=10.0, type=float, help="time step for controls")
-    parser.add_argument("--time", default=200.0, type=float, help="gate time")
+    parser.add_argument("--max_amp", default=[0.003, 0.05, 0.05], help="max drive amp in GHz")
+    parser.add_argument("--dt", default=5.0, type=float, help="time step for controls")
+    parser.add_argument("--time", default=140.0, type=float, help="gate time")
     parser.add_argument("--scale", default=0.001, type=float, help="randomization scale for initial pulse")
-    parser.add_argument("--learning_rate", default=0.0001, type=float, help="learning rate for ADAM optimize")
+    parser.add_argument("--learning_rate", default=0.0005, type=float, help="learning rate for ADAM optimize")
     parser.add_argument("--b1", default=0.999, type=float, help="decay of learning rate first moment")
     parser.add_argument("--b2", default=0.999, type=float, help="decay of learning rate second moment")
     parser.add_argument("--coherent", default=1, type=int, help="which fidelity metric to use")
-    parser.add_argument("--epochs", default=1000, type=int, help="number of epochs")
+    parser.add_argument("--epochs", default=10000, type=int, help="number of epochs")
     parser.add_argument("--target_fidelity", default=0.9995, type=float, help="target fidelity")
     parser.add_argument("--rng_seed", default=87336259, type=int, help="rng seed for random initial pulses")
     parser.add_argument("--plot", default=True, type=bool, help="plot the results?")
@@ -49,9 +50,12 @@ if __name__ == "__main__":
 
     a = tensor(destroy(args.c_dim), eye(args.t_dim))
     b = tensor(eye(args.c_dim), destroy(args.t_dim))
-    H0 = (-2.0 * jnp.pi * args.Kerr * 0.5 * dag(b) @ dag(b) @ b @ b
-          -2.0 * jnp.pi * 0.00 * dag(a) @ a @ dag(b) @ b)
+    H0 = -2.0 * jnp.pi * args.Kerr * 0.5 * dag(b) @ dag(b) @ b @ b
     H1 = [dag(a) @ a @ dag(b) @ b, b + dag(b), 1j * (b - dag(b))]
+    if type(args.max_amp) is float:
+        max_amp = len(H1) * [2.0 * jnp.pi * args.max_amp]
+    else:
+        max_amp = 2.0 * jnp.pi * jnp.asarray(args.max_amp)
     if args.gate == "error_parity_g":
         initial_states = [tensor(basis(args.c_dim, c_idx), basis(args.t_dim, 0))
                           for c_idx in range(args.c_dim)]
@@ -72,18 +76,14 @@ if __name__ == "__main__":
     def H_func(t, drive_params):
         H = H0
         for drive_idx in range(len(H1)):
-            if drive_idx == 0:
-                total_drive = jnp.clip(
-                    envelope * drive_params[drive_idx],
-                    a_min=-2.0 * np.pi * args.chi_max,
-                    a_max=2.0 * np.pi * args.chi_max,
-                )
-            else:
-                total_drive = envelope * drive_params[drive_idx]
-            drive_spline = InterpolatedUnivariateSpline(
-                tsave, total_drive, endpoints="natural"
+            total_drive = jnp.clip(
+                envelope * drive_params[drive_idx],
+                a_min=-max_amp[drive_idx],
+                a_max=max_amp[drive_idx],
             )
-            H = H + drive_spline(t) * H1[drive_idx]
+            drive_coeffs = dx.backward_hermite_coefficients(tsave, total_drive)
+            drive_spline = dx.CubicInterpolation(tsave, drive_coeffs)
+            H = H + drive_spline.evaluate(t) * H1[drive_idx]
         return H
 
     H_tc = timecallable(H_func, args=(init_drive_params,))
@@ -103,21 +103,54 @@ if __name__ == "__main__":
         finer_times = jnp.linspace(0.0, args.time, 201)
         fig, ax = plt.subplots()
         for drive_idx in range(len(H1)):
-            if drive_idx == 0:
-                total_drive = jnp.clip(
-                    envelope * opt_params[drive_idx],
-                    a_min=-2.0 * np.pi * args.chi_max,
-                    a_max=2.0 * np.pi * args.chi_max,
-                )
-            else:
-                total_drive = envelope * opt_params[drive_idx]
-            drive_spline = InterpolatedUnivariateSpline(
-                tsave, total_drive, endpoints="natural"
+            total_drive = jnp.clip(
+                envelope * opt_params[drive_idx],
+                a_min=-max_amp[drive_idx],
+                a_max=max_amp[drive_idx],
             )
-            plt.plot(finer_times, drive_spline(finer_times)/(2.0 * np.pi), label=f"I_{drive_idx}")
+            drive_coeffs = dx.backward_hermite_coefficients(tsave, total_drive)
+            drive_spline = dx.CubicInterpolation(tsave, drive_coeffs)
+            plt.plot(finer_times, drive_spline.evaluate(finer_times)/(2.0 * np.pi), label=f"I_{drive_idx}")
+        plt.plot(finer_times, (np.pi / (2.0 * np.pi * tsave[-1])) * jnp.ones_like(finer_times),
+                 ls="--", color="black", label="chi")
+        plt.plot(finer_times, (-np.pi / (2.0 * np.pi * tsave[-1])) * jnp.ones_like(finer_times),
+                 ls="--", color="black")
         ax.set_xlabel("time [ns]")
         ax.set_ylabel("pulse amplitude [GHz]")
         ax.legend()
         plt.tight_layout()
         plt.savefig(filename[:-5]+"_pulse.pdf")
+        plt.show()
+
+        def Pij(c_idx, t_idx):
+            ket = tensor(basis(args.c_dim, c_idx), basis(args.t_dim, t_idx))
+            return ket @ dag(ket)
+
+        H_opt = H_tc = timecallable(H_func, args=(opt_params,))
+        result = sesolve(H_tc, initial_states, finer_times,
+                         exp_ops=[Pij(c_idx, t_idx)
+                                  for c_idx in range(args.c_dim)
+                                  for t_idx in range(args.t_dim)])
+        fig, ax = plt.subplots()
+        state_idx = 0
+        for c_idx in range(args.c_dim):
+            for t_idx in range(args.t_dim):
+                plt.plot(finer_times, result.expects[0, state_idx],
+                         label=f"|{c_idx},{t_idx}>")
+                state_idx += 1
+        ax.legend()
+        ax.set_xlabel("time [ns]")
+        ax.set_ylabel("population")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        state_idx = 0
+        for c_idx in range(args.c_dim):
+            for t_idx in range(args.t_dim):
+                plt.plot(finer_times, result.expects[1, state_idx],
+                         label=f"|{c_idx},{t_idx}>")
+                state_idx += 1
+        ax.legend()
+        ax.set_xlabel("time [ns]")
+        ax.set_ylabel("population")
         plt.show()
