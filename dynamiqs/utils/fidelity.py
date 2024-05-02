c05221de5c92904ad99d785257811e026951ce84
Danny <dkweiss@u.northwestern.edu>
2024-05-02 10:56:51 -0400

    helper fidelity function gf stuff for dynamic chi

--- dynamiqs/utils/fidelity.py
+++ dynamiqs/utils/fidelity.py
@@ -5,9 +5,10 @@ import jax.numpy as jnp
 __all__ = [
     'infidelity_coherent',
     'infidelity_incoherent',
+    'all_X_Y_Z_states'
 ]
 
-from dynamiqs import isket, isbra, toket
+from dynamiqs import isket, isbra, toket, unit
 
 
 def _overlaps(computed_states, target_states):
@@ -38,3 +39,14 @@ def infidelity_incoherent(computed_states, target_states):
     overlaps_sq = jnp.abs(overlaps * jnp.conj(overlaps))
     fids = jnp.mean(overlaps_sq, axis=-1)
     return 1 - fids
+
+
+def all_X_Y_Z_states(basis_states):
+    all_states = []
+    for idx_1, state_1 in enumerate(basis_states):
+        all_states.append(state_1)
+        for idx_2, state_2 in enumerate(basis_states):
+            if idx_2 > idx_1:
+                all_states.append(unit(state_1 + state_2))
+                all_states.append(unit(state_1 + 1j * state_2))
+    return all_states
--- examples/run_dynamic_chi.py
+++ examples/run_dynamic_chi.py
@@ -6,33 +6,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import optax
 
-from dynamiqs import Options, grape, timecallable, dag, tensor, basis, destroy, eye, unit
+from dynamiqs import Options, grape, timecallable, dag, tensor, basis, destroy, eye, unit, mcsolve
 from dynamiqs import generate_noise_trajectory
 from dynamiqs import sesolve
+from dynamiqs.utils.fidelity import all_X_Y_Z_states
 from dynamiqs.utils.file_io import generate_file_path
 import diffrax as dx
+from cycler import cycler
+
+color_cycler = plt.rcParams['axes.prop_cycle']
+ls_cycler = cycler(ls=['-', '--', '-.', ':'])
+alpha_cycler = cycler(alpha=[1.0, 0.6, 0.2])
+lw_cycler = cycler(lw=[2.0, 1.0])
+color_ls_alpha_cycler = alpha_cycler * lw_cycler * ls_cycler * color_cycler
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="dynamic chi sim")
     parser.add_argument("--idx", default=-1, type=int, help="idx to scan over")
-    parser.add_argument("--gate", default="error_parity_plus", type=str,
+    parser.add_argument("--gate", default="error_parity_plus_gf", type=str,
                         help="type of gate. Can be error_parity_g, error_parity_plus, ...")
+    parser.add_argument("--grape_type", default="jumps", type=str, help="can be unitary, jumps or unitary_and_jumps")
     parser.add_argument("--c_dim", default=4, type=int, help="cavity hilbert dim cutoff")
-    parser.add_argument("--t_dim", default=4, type=int, help="tmon hilbert dim cutoff")
+    parser.add_argument("--t_dim", default=3, type=int, help="tmon hilbert dim cutoff")
     parser.add_argument("--Kerr", default=0.100, type=float, help="transmon Kerr in GHz")
-    parser.add_argument("--max_amp", default=[0.001, 0.05, 0.05], help="max drive amp in GHz")
-    parser.add_argument("--dt", default=25.0, type=float, help="time step for controls")
-    parser.add_argument("--time", default=1000.0, type=float, help="gate time")
-    parser.add_argument("--ramp_nts", default=2, type=int, help="numper of points in ramps")
+    parser.add_argument("--max_amp", default=[0.001, 0.002, 0.05, 0.05], help="max drive amp in GHz")
+    parser.add_argument("--dt", default=20.0, type=float, help="time step for controls")
+    parser.add_argument("--time", default=700.0, type=float, help="gate time")
+    parser.add_argument("--ramp_nts", default=4, type=int, help="numper of points in ramps")
     parser.add_argument("--scale", default=1e-5, type=float, help="randomization scale for initial pulse")
-    parser.add_argument("--learning_rate", default=0.001, type=float, help="learning rate for ADAM optimize")
+    parser.add_argument("--learning_rate", default=0.0005, type=float, help="learning rate for ADAM optimize")
     parser.add_argument("--b1", default=0.999, type=float, help="decay of learning rate first moment")
     parser.add_argument("--b2", default=0.999, type=float, help="decay of learning rate second moment")
-    parser.add_argument("--coherent", default=1, type=int, help="which fidelity metric to use")
-    parser.add_argument("--epochs", default=300, type=int, help="number of epochs")
-    parser.add_argument("--target_fidelity", default=0.9990, type=float, help="target fidelity")
-    parser.add_argument("--rng_seed", default=46, type=int, help="rng seed for random initial pulses")  # 87336259
+    parser.add_argument("--coherent", default=0, type=int, help="which fidelity metric to use")
+    parser.add_argument("--epochs", default=2000, type=int, help="number of epochs")
+    parser.add_argument("--target_fidelity", default=0.990, type=float, help="target fidelity")
+    parser.add_argument("--rng_seed", default=854, type=int, help="rng seed for random initial pulses")  # 87336259
     parser.add_argument("--include_low_frequency_noise", default=0, type=int,
                         help="whether to batch over different realizations of low-frequency noise")
     parser.add_argument("--num_freq_shift_trajs", default=5, type=int,
@@ -42,6 +51,9 @@ if __name__ == "__main__":
                         help="std-dev of frequency shifts given by sqrt(relative_PSD_strength * sample_rate)")
     parser.add_argument("--f0", default=1e-2, type=float, help="cutoff frequency for 1/f noise (in us^-1)")
     parser.add_argument("--white", default=0, type=int, help="white or 1/f noise")
+    parser.add_argument("--T1", default=10000, type=float, help="T1 of the transmon in ns. If not infinity, "
+                                                                 "includes jumps")
+    parser.add_argument("--ntraj", default=20, type=int, help="number of jump trajectories")
     parser.add_argument("--plot", default=True, type=bool, help="plot the results?")
     parser_args = parser.parse_args()
     if parser_args.idx == -1:
@@ -49,6 +61,7 @@ if __name__ == "__main__":
     else:
         filename = f"out/{str(parser_args.idx).zfill(5)}_dynamic_chi_{parser_args.gate}.h5py"
     c_dim = parser_args.c_dim
+    t_dim = parser_args.t_dim
 
     optimizer = optax.adam(learning_rate=parser_args.learning_rate, b1=parser_args.b1, b2=parser_args.b2)
     ntimes = int(parser_args.time // parser_args.dt) + 1
@@ -62,30 +75,69 @@ if __name__ == "__main__":
         coherent = False
     else:
         coherent = True
-    options = Options(target_fidelity=parser_args.target_fidelity, epochs=parser_args.epochs, coherent=coherent)
+    options = Options(
+        save_states=False,
+        target_fidelity=parser_args.target_fidelity,
+        epochs=parser_args.epochs,
+        coherent=coherent,
+        ntraj=parser_args.ntraj,
+        one_jump_only=True,
+    )
 
-    a = tensor(destroy(parser_args.c_dim), eye(parser_args.t_dim))
-    b = tensor(eye(parser_args.c_dim), destroy(parser_args.t_dim))
-    H0 = -2.0 * jnp.pi * parser_args.Kerr * 0.5 * dag(b) @ dag(b) @ b @ b
-    H1 = [dag(a) @ a @ dag(b) @ b, b + dag(b), 1j * (b - dag(b))]
+    a = tensor(destroy(c_dim), eye(t_dim))
+    b = tensor(eye(c_dim), destroy(t_dim))
+    g_proj = tensor(eye(c_dim), basis(t_dim, 0) @ dag(basis(t_dim, 0)))
+    e_proj = tensor(eye(c_dim), basis(t_dim, 1) @ dag(basis(t_dim, 1)))
+    f_proj = tensor(eye(c_dim), basis(t_dim, 2) @ dag(basis(t_dim, 2)))
+    gf_proj = tensor(eye(c_dim), basis(t_dim, 0) @ dag(basis(t_dim, 2)))
+    ge_proj = tensor(eye(c_dim), basis(t_dim, 0) @ dag(basis(t_dim, 1)))
+    ef_proj = tensor(eye(c_dim), basis(t_dim, 1) @ dag(basis(t_dim, 2)))
+    # H0 = -2.0 * jnp.pi * parser_args.Kerr * 0.5 * dag(b) @ dag(b) @ b @ b
+    H0 = 0.0 * b
+    H1 = [dag(a) @ a @ e_proj, dag(a) @ a @ f_proj, gf_proj + dag(gf_proj), 1j * (gf_proj - dag(gf_proj))]
+    # H1 = [dag(a) @ a @ e_proj, dag(a) @ a @ f_proj, b + dag(b), 1j * (b - dag(b))]
+    # H1 = [dag(a) @ a @ dag(b) @ b, b + dag(b), 1j * (b - dag(b))]
     # H1 = [dag(a) @ a @ dag(b) @ b, ]
+    if parser_args.T1 != np.inf:
+        jump_ops = [jnp.sqrt(1. / parser_args.T1) * b, ]
+    else:
+        jump_ops = None
     if type(parser_args.max_amp) is float:
         max_amp = len(H1) * [2.0 * jnp.pi * parser_args.max_amp]
-    else:
+    elif len(parser_args.max_amp) == len(H1):
         max_amp = 2.0 * jnp.pi * jnp.asarray(parser_args.max_amp)
+    else:
+        raise RuntimeError("max_amp needs to be a float or have the same dimension as H1")
     if parser_args.gate == "error_parity_g":
-        initial_states = [tensor(basis(parser_args.c_dim, c_idx), basis(parser_args.t_dim, 0))
+        initial_states = [tensor(basis(c_dim, c_idx), basis(t_dim, 0))
                           for c_idx in range(2)]
-        final_states = [tensor(basis(parser_args.c_dim, c_idx), basis(parser_args.t_dim, c_idx % 2))
+        final_states = [tensor(basis(c_dim, c_idx), basis(t_dim, c_idx % 2))
                         for c_idx in range(2)]
+        final_states_traj = None
     elif parser_args.gate == "error_parity_plus":
-        initial_states = [tensor(basis(parser_args.c_dim, c_idx), unit(basis(parser_args.t_dim, 0) + basis(parser_args.t_dim, 1)))
+        initial_states = [tensor(basis(c_dim, c_idx), unit(basis(t_dim, 0) + basis(t_dim, 1)))
                           for c_idx in range(2)]
-        final_states = [tensor(basis(parser_args.c_dim, c_idx),
-                               unit(basis(parser_args.t_dim, 0) + (-1) ** (c_idx % 2) * basis(parser_args.t_dim, 1)))
+        final_states = [tensor(basis(c_dim, c_idx),
+                               unit(basis(t_dim, 0) + (-1) ** (c_idx % 2) * basis(t_dim, 1)))
                         for c_idx in range(2)]
+        final_states_traj = None
+    elif parser_args.gate == "error_parity_plus_gf":
+        initial_states = [tensor(basis(c_dim, c_idx), unit(basis(t_dim, 0) + basis(t_dim, 2)))
+                          for c_idx in range(2)]
+        final_states = [tensor(basis(c_dim, c_idx),
+                               unit(basis(t_dim, 0) + (-1) ** (c_idx % 2) * basis(t_dim, 2)))
+                        for c_idx in range(2)]
+        final_states_traj = [
+            tensor(basis(c_dim, c_idx), basis(t_dim, 1)) for c_idx in range(2)
+        ]
     else:
         raise RuntimeError("gate type not supported")
+
+    if parser_args.coherent == 0:
+        initial_states = all_X_Y_Z_states(initial_states)
+        final_states = all_X_Y_Z_states(final_states)
+        if final_states_traj is not None:
+            final_states_traj = all_X_Y_Z_states(final_states_traj)
 
     if parser_args.include_low_frequency_noise:
 
@@ -139,6 +191,9 @@ if __name__ == "__main__":
         target_states=final_states,
         tsave=tsave,
         params_to_optimize=init_drive_params,
+        grape_type=parser_args.grape_type,
+        jump_ops=jump_ops,
+        target_states_traj=final_states_traj,
         additional_drive_args=additional_drive_args,
         filepath=filename,
         optimizer=optimizer,
@@ -171,34 +226,48 @@ if __name__ == "__main__":
         plt.show()
 
         def Pij(c_idx, t_idx):
-            ket = tensor(basis(parser_args.c_dim, c_idx), basis(parser_args.t_dim, t_idx))
+            ket = tensor(basis(c_dim, c_idx), basis(t_dim, t_idx))
             return ket @ dag(ket)
 
         H_opt = H_tc = timecallable(H_func, args=(opt_params, 0))
-        result = sesolve(H_tc, initial_states, finer_times,
-                         exp_ops=[Pij(c_idx, t_idx)
-                                  for c_idx in range(parser_args.c_dim)
-                                  for t_idx in range(parser_args.t_dim)])
-        fig, ax = plt.subplots()
-        state_idx = 0
-        for c_idx in range(parser_args.c_dim):
-            for t_idx in range(parser_args.t_dim):
-                plt.plot(finer_times, result.expects[0, state_idx],
-                         label=f"|{c_idx},{t_idx}>")
-                state_idx += 1
-        ax.legend()
-        ax.set_xlabel("time [ns]")
-        ax.set_ylabel("population")
-        plt.show()
+        if parser_args.gate == "error_parity_plus_gf":
+            e_idx = 2
+        else:
+            e_idx = 1
 
-        fig, ax = plt.subplots()
-        state_idx = 0
-        for c_idx in range(parser_args.c_dim):
-            for t_idx in range(parser_args.t_dim):
-                plt.plot(finer_times, result.expects[1, state_idx],
-                         label=f"|{c_idx},{t_idx}>")
-                state_idx += 1
-        ax.legend()
-        ax.set_xlabel("time [ns]")
-        ax.set_ylabel("population")
-        plt.show()
+        X_ops = [
+            tensor(basis(c_dim, c_idx), basis(t_dim, 0)) @ dag(tensor(basis(c_dim, c_idx), basis(t_dim, e_idx)))
+            + tensor(basis(c_dim, c_idx), basis(t_dim, e_idx)) @ dag(tensor(basis(c_dim, c_idx), basis(t_dim, 0)))
+            for c_idx in range(c_dim)
+        ]
+        X_labels = [f"X_{c_idx}" for c_idx in range(c_dim)]
+        Y_ops = [
+            1j * tensor(basis(c_dim, c_idx), basis(t_dim, 0)) @ dag(tensor(basis(c_dim, c_idx), basis(t_dim, e_idx)))
+            - 1j * tensor(basis(c_dim, c_idx), basis(t_dim, e_idx)) @ dag(tensor(basis(c_dim, c_idx), basis(t_dim, 0)))
+            for c_idx in range(c_dim)
+        ]
+        Y_labels = [f"Y_{c_idx}" for c_idx in range(c_dim)]
+        Z_ops = [
+            tensor(basis(c_dim, c_idx), basis(t_dim, 0)) @ dag(tensor(basis(c_dim, c_idx), basis(t_dim, 0)))
+            - tensor(basis(c_dim, c_idx), basis(t_dim, e_idx)) @ dag(tensor(basis(c_dim, c_idx), basis(t_dim, e_idx)))
+            for c_idx in range(c_dim)
+        ]
+        exp_ops = X_ops+Y_ops+Z_ops
+        Z_labels = [f"Z_{c_idx}" for c_idx in range(c_dim)]
+        labels = X_labels + Y_labels + Z_labels
+
+        if parser_args.T1 != np.inf:
+            result = mcsolve(H_tc, jump_ops, initial_states, finer_times, exp_ops=exp_ops)
+        else:
+            result = sesolve(H_tc, initial_states, finer_times, exp_ops=exp_ops)
+
+        for state_idx in range(len(initial_states)):
+            fig, ax = plt.subplots()
+            expects = result.expects[state_idx]
+            for e_result, label, sty in zip(expects, labels, color_ls_alpha_cycler):
+                plt.plot(finer_times, e_result, label=label, **sty)
+            ax.legend()
+            ax.set_xlabel("time [ns]")
+            ax.set_ylabel("population")
+            ax.set_title(f"state_idx={state_idx}")
+            plt.show()
