a1b8a323a56d9f1d1dba6ab75aebffd02f467b90
Danny <dkweiss@u.northwestern.edu>
2024-05-02 10:55:38 -0400

    added mcsolve capabilites to grape

--- dynamiqs/grape/grape.py
+++ dynamiqs/grape/grape.py
@@ -28,6 +28,7 @@ def grape(
     tsave: ArrayLike,
     params_to_optimize: ArrayLike,
     *,
+    grape_type="unitary",
     jump_ops=None,
     target_states_traj: ArrayLike = None,
     additional_drive_args: ArrayLike = None,
@@ -51,6 +52,10 @@ def grape(
              tsave _(array-like of shape (nt,))_: times to be passed to sesolve
              params_to_optimize _(dict or array-like)_: parameters to optimize
                 over that are used to define the Hamiltonian
+             grape_type _(str)_: can be unitary, jumps, unitary_and_jumps. In the first
+                case, just do sesolve dynamics. In the second just do mcsolve dynamics.
+                In the third we can optimize over a combination of the unitary dynamics
+                and jump dynamics. In this case we ignore the no-jump trajectories (redundant)
              filepath _(str)_: filepath of where to save optimization results
              optimizer _(optax.GradientTransformation)_: optax optimizer to use
                 for gradient descent. Defaults to the Adam optimizer
@@ -88,6 +93,7 @@ def grape(
                 solver,
                 options,
                 optimizer,
+                grape_type,
             )
             data_dict = {"infidelities": infids}
             save_and_print(
@@ -132,7 +138,7 @@ def save_and_print(
         write_to_h5_multi(filepath, data_dict, init_param_dict)
 
 
-@partial(jax.jit, static_argnames=("solver", "options", "optimizer"))
+@partial(jax.jit, static_argnames=("solver", "options", "optimizer", "grape_type"))
 def step(
     params_to_optimize,
     opt_state,
@@ -146,6 +152,7 @@ def step(
     solver,
     options,
     optimizer,
+    grape_type,
 ):
     """calculate gradient of the loss and step updated parameters.
     We have has_aux=True because loss also returns the infidelities on the side
@@ -160,7 +167,8 @@ def step(
         tsave,
         additional_drive_args,
         solver,
-        options
+        options,
+        grape_type,
     )
     updates, opt_state = optimizer.update(grads, opt_state)
     params_to_optimize = optax.apply_updates(params_to_optimize, updates)
@@ -177,7 +185,66 @@ def loss(
     tsave,
     additional_drive_args,
     solver,
-    options
+    options,
+    grape_type,
+):
+    if options.coherent:
+        infid_func = infidelity_coherent
+    else:
+        infid_func = infidelity_incoherent
+    if grape_type == "unitary" or grape_type == "unitary_and_jumps":
+        unitary_infids = _unitary_infids(
+            params_to_optimize,
+            H,
+            initial_states,
+            target_states,
+            tsave,
+            additional_drive_args,
+            solver,
+            options,
+            infid_func,
+        )
+        if grape_type == "unitary":
+            return jnp.log(jnp.sum(unitary_infids)), unitary_infids
+    elif grape_type == "jumps" or grape_type == "unitary_and_jumps":
+        if grape_type == "jumps":
+            # don't care about the unitary part, weight jump and no jump equally
+            jump_no_jump_weights = jnp.array([1.0, 1.0])
+        else:
+            # otherwise we are doing unitary_and_jumps so don't care about no jump
+            jump_no_jump_weights = jnp.array([1.0, 0.0])
+        traj_infids = _traj_infids(
+            params_to_optimize,
+            H,
+            jump_ops,
+            initial_states,
+            target_states,
+            target_states_traj,
+            tsave,
+            jump_no_jump_weights,
+            solver,
+            options,
+            infid_func,
+        )
+        if grape_type == "jumps":
+            return jnp.log(jnp.sum(traj_infids)), traj_infids
+    else:
+        raise RuntimeError(f"grape_type must be unitary, jumps or unitary_and_jumps but got {grape_type}")
+    # if we made it here then grape_type==unitary_and_jumps
+    infids = jnp.concatenate((unitary_infids, traj_infids))
+    return jnp.log(jnp.sum(infids)), infids
+
+
+def _unitary_infids(
+    params_to_optimize,
+    H,
+    initial_states,
+    target_states,
+    tsave,
+    additional_drive_args,
+    solver,
+    options,
+    infid_func,
 ):
     # update H using the same function but new parameters
     if additional_drive_args is not None:
@@ -190,30 +257,44 @@ def loss(
     else:
         H = timecallable(H.f, args=(params_to_optimize, 0))
         results = dq.sesolve(H, initial_states, tsave, solver=solver, options=options)
-    final_state = results.final_state
-    if options.coherent:
-        infids = infidelity_coherent(final_state, target_states)
-    else:
-        infids = infidelity_incoherent(final_state, target_states)
-    if jump_ops is not None:
-        H = timecallable(H.f, args=(params_to_optimize, 0))
-        mcsolve_results = dq.mcsolve(H, jump_ops, initial_states, tsave, solver=solver, options=options)
-        final_state = dq.unit(mcsolve_results.final_jump_states).swapaxes(0, 1)
-        # assuming we want same final states as without jumps
-        # annoying code repetition
-        if options.coherent:
-            infids = jnp.concatenate((
-                infids, infidelity_coherent(
-                    final_state, target_states_traj
-                )
-            ))
-        else:
-            infids = jnp.concatenate((
-                infids, infidelity_incoherent(
-                    mcsolve_results.final_jump_states, target_states_traj
-                )
-            ))
+    infids = infid_func(results.final_state, target_states)
     if infids.ndim == 0:
         # for saving purposes, want this to be an Array as opposed to a float
         infids = infids[None]
-    return jnp.sum(jnp.log(infids)), infids
+    return infids
+
+
+def _traj_infids(
+    params_to_optimize,
+    H,
+    jump_ops,
+    initial_states,
+    target_states,
+    target_states_jump,
+    tsave,
+    jump_no_jump_weights,
+    solver,
+    options,
+    infid_func,
+):
+    # right now don't allow additional_drive_args, could allow in the future
+    H = timecallable(H.f, args=(params_to_optimize, 0))
+    mcsolve_results = dq.mcsolve(H, jump_ops, initial_states, tsave, solver=solver, options=options)
+    final_jump_states = dq.unit(mcsolve_results.final_jump_states).swapaxes(0, 1)
+    final_no_jump_states = dq.unit(mcsolve_results.final_no_jump_state)
+    infids_jump = infid_func(
+        final_jump_states, target_states_jump
+    )
+    infids_jump_avg = jnp.mean(infids_jump)
+    infids_no_jump = infid_func(
+        final_no_jump_states, target_states
+    )
+    if infids_jump_avg.ndim == 0:
+        # for saving purposes, want this to be an Array as opposed to a float
+        infids_jump_avg = infids_jump_avg[None]
+    if infids_no_jump.ndim == 0:
+        # for saving purposes, want this to be an Array as opposed to a float
+        infids_no_jump = infids_no_jump[None]
+    infids = jnp.concatenate((jump_no_jump_weights[0] * infids_jump_avg,
+                              jump_no_jump_weights[1] * infids_no_jump))
+    return infids
