cf4ba35fba451cc4071e75cf737e277df868ed14
Danny <dkweiss@u.northwestern.edu>
2024-06-10 16:53:57 -0400

    Revert "New progress bar format (#589)"
    This reverts commit a75516d9c72850b3ae514157c4d895c9b315dc9c.

--- dynamiqs/progress_meter.py
+++ dynamiqs/progress_meter.py
@@ -2,7 +2,6 @@ from abc import abstractmethod
 
 import diffrax as dx
 import equinox as eqx
-import tqdm
 
 __all__ = [
     'AbstractProgressMeter',
@@ -28,15 +27,6 @@ class TextProgressMeter(AbstractProgressMeter):
         return dx.TextProgressMeter()
 
 
-class _DiffraxTqdmProgressMeter(dx.TqdmProgressMeter):
-    @staticmethod
-    def _init_bar() -> tqdm.tqdm:
-        bar_format = (
-            '|{bar}| {percentage:5.1f}% ◆ total {elapsed} ◆ remaining {remaining}'
-        )
-        return tqdm.tqdm(total=100, unit='%', bar_format=bar_format)
-
-
 class TqdmProgressMeter(AbstractProgressMeter):
     def to_diffrax(self) -> dx.AbstractProgressMeter:
-        return _DiffraxTqdmProgressMeter()
+        return dx.TqdmProgressMeter()
