7abc88144cfcae2f5dc3ed229fde6d0823a11f3e
Danny <dkweiss@u.northwestern.edu>
2024-07-25 20:53:13 -0400

    y0 already defined in sepropagator call, so use flat vectorize instead

--- dynamiqs/integrators/sepropagator/dynamiqs_integrator.py
+++ dynamiqs/integrators/sepropagator/dynamiqs_integrator.py
@@ -5,7 +5,7 @@ from jaxtyping import PyTree
 from ...result import Saved, SEResult
 from ...time_array import Shape
 from ...utils.operators import eye
-from .._utils import _cartesian_vectorize
+from .._utils import _cartesian_vectorize, _flat_vectorize
 from ..apis.sesolve import _sesolve
 from ..core.abstract_integrator import SEPropagatorIntegrator
 
@@ -16,13 +16,20 @@ class SEPropagatorDynamiqsIntegrator(SEPropagatorIntegrator):
         # product, so rather than directly calling sesolve on batched input
         # states, we call _sesolve instead and specify the initial state as a
         # square matrix (the identity matrix).
-        initial_states = eye(self.H.shape[-1])
-        n_batch = (self.H.in_axes, Shape(), Shape(), Shape(), Shape(), Shape(), Shape())
+        n_batch = (
+            self.H.in_axes,
+            Shape(self.y0.shape[:-2]),
+            Shape(),
+            Shape(),
+            Shape(),
+            Shape(),
+            Shape()
+        )
         out_axes = SEResult(False, False, False, False, 0, 0)
-        f = _cartesian_vectorize(_sesolve, n_batch, out_axes)
+        f = _flat_vectorize(_sesolve, n_batch, out_axes)
         seresult = f(
             self.H,
-            initial_states,
+            self.y0,
             self.ts,
             None,
             self.solver,
